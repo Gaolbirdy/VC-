33782f92d8f991188df4a995cab4f8d54bab89b2
Gaolbirdy <gaolbird@yeah.net>
2017-12-19 23:58:17 +0800

    commit

---
+++ SIKI/04-第二个HelloWorld程序/HelloWorld.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+
+/*
+这里
+都是
+注释
+*/
+
+// 函数function
+int main() 
+{
+	cout << "Hello World" << endl;	// 这个是用来向控制台输出一段字符串
+	//std::cout << "Hello World" << std::endl;
+
+	return 0;	// 返回值
+}
---
+++ SIKI/05-数据类型、常量和变量/05数据类型常量和变量.cpp
@@ -0,0 +1,38 @@
+// 05数据类型常量和变量.cpp: 定义控制台应用程序的入口点。
+//
+
+#include "stdafx.h"
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main()
+{
+	//cout << 100 << endl;
+	//cout << 1.1 << endl;
+	//cout << 'a' << endl;
+	//cout << ' ' << endl;
+	//cout << "www.sikiedu.com" << endl;	// end line
+	
+	//int age = 100;	// 赋值
+	//cout << age << endl;
+	//age = 99;
+	//cout << age << endl;
+	//age = -1000000;
+
+	/*int age = 100;
+	float hp = 1.1;
+	double hp2 = 1.1;
+	char c = 'a';
+	string name = "siki";
+	cout << age << endl;
+	cout << hp << endl;
+	cout << hp2 << endl;
+	cout << c << endl;
+	cout << name << endl;*/
+
+	cout << "Hi" << endl << endl << "popo" << endl;
+	cout << "Hi\n\ni\n\n";
+
+	return 0;
+}
