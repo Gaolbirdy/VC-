da3cf0cd303355188de2c867cdc04675fd08c31a
liukuo <liukuo@gitlab.com>
2017-12-21 08:44:25 +0800

    commit

---
+++ RUNOOB/教程/教程.cpp
@@ -0,0 +1,16 @@
+// 教程.cpp: 定义控制台应用程序的入口点。
+//
+
+#include "stdafx.h"
+#include <iostream>
+//using namespace std;
+
+// main() 是程序开始执行的地方
+int main()
+{
+	//cout << "Hello,\nWorld!" << endl;
+	std::cout << "Hello,\nWorld!" << std::endl;
+
+	return 0;
+}
+
