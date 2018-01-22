f0d15feb0fe33762abda1f8cc531faf2a1c582ab
liukuo <liukuo@gitlab.com>
2018-01-22 08:50:00 +0800

    commit

--- RUNOOB/信号处理/信号处理.cpp
+++ RUNOOB/信号处理/信号处理.cpp
@@ -3,11 +3,49 @@
 
 #include "stdafx.h"
 #include <iostream>
+#include <csignal>
+//#include <unistd.h>
+#include <windows.h>
 
 using namespace std;
 
-int main()
+void signalHandler(int signum)
 {
-    return 0;
+	cout << "Interrupt signal (" << signum << ") received.\n";
+
+	// 清理并关闭
+	// 终止程序 
+	exit(signum);
+}
+
+//int main()
+//{
+//	// 注册信号 SIGINT 和信号处理程序
+//	signal(SIGINT, signalHandler);
+//
+//	while (1)
+//	{
+//		cout << "Going to sleep...." << endl;
+//		Sleep(1000);
+//	}
+//	
+//	return 0;
+//}
+
+void main()
+{
+	int i = 0;
+	// 注册信号 SIGINT 和信号处理程序
+	signal(SIGINT, signalHandler);
+
+	while (++i)
+	{
+		cout << "Going to sleep...." << endl;
+		if (i == 3)
+		{
+			raise(SIGINT);
+		}
+		Sleep(1000);
+	}
 }
 
