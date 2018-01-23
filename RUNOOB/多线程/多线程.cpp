ae0daf5863433034511cb2e0a27d17e14c00cd65
liukuo <liukuo@gitlab.com>
2018-01-23 08:02:57 +0800

    commit

--- RUNOOB/多线程/多线程.cpp
+++ RUNOOB/多线程/多线程.cpp
@@ -132,5 +132,33 @@ void main()
 
 	// 初始化并设置线程为可连接的（joinable）
 	pthread_attr_init(&attr);
+	pthread_attr_setdetachstate(&attr, PTHREAD_CREATE_JOINABLE);
 
+	for (i = 0; i < NUM_THREADS; i++)
+	{
+		cout << "main(): creating thread, " << i << endl;
+		rc = pthread_create(&threads[i], NULL, wait, (void*)&i);
+		if (rc)
+		{
+			cout << "Error: unable to create thread," << rc << endl;
+			exit(-1);
+		}
+	}
+
+	// 删除属性，并等待其他线程
+	pthread_attr_destroy(&attr);
+	for (i = 0; i < NUM_THREADS; i++)
+	{
+		rc = pthread_join(threads[i], &status);
+		if (rc)
+		{
+			cout << "Error: unable to join," << rc << endl;
+			exit(-1);
+		}
+		cout << "Main: completed thread id : " << i;
+		cout << "  exiting with status :" << status << endl;
+	}
+
+	cout << "Main: program exiting." << endl;
+	pthread_exit(NULL);
 }
